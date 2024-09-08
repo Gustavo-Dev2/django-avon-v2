0fcc549f0d9dd45fe5989280646645c8e5fd6c1a
PicoHBK <picohbk322@gmai.com>
2024-09-08 14:44:29 -0300

    cors v2

--- server/settings.py
+++ server/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ALLOWED_ORIGIN = [
     'http://localhost:5173'
